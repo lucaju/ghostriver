dfd61944bc544acdb889000acef67310fb7414ec
Luciano Frizzera <lucaju@me.com>
2019-08-24 11:22:21 -0400

    clean code

--- src/tags.js
+++ src/tags.js
@@ -7,16 +7,18 @@ import speculative from './assets/help.svg';
 import beyondHumans from './assets/snake.svg';
 import disreappearingWaters from './assets/sea.svg';
 
+
 export const getIcon = ({slug}) => {
-	if (slug.toLowerCase() == 'ghosts') return ghost;
-	if (slug.toLowerCase() == 'unrullywaters') return unrulyWaters;
-	if (slug.toLowerCase() == 'imaginaries') return imaginaries;
-	if (slug.toLowerCase() == 'infrastructure') return infrastructures;
-	if (slug.toLowerCase() == 'contamination') return contamination;
-	if (slug.toLowerCase() == 'speculation') return speculative;
-	if (slug.toLowerCase() == 'beyond-humans') return beyondHumans;
-	if (slug.toLowerCase() == 'disreappearingwaters') return disreappearingWaters;
+	if (slug.toLowerCase() === 'ghosts') return ghost;
+	if (slug.toLowerCase() === 'unrullywaters') return unrulyWaters;
+	if (slug.toLowerCase() === 'imaginaries') return imaginaries;
+	if (slug.toLowerCase() === 'infrastructure') return infrastructures;
+	if (slug.toLowerCase() === 'contamination') return contamination;
+	if (slug.toLowerCase() === 'speculation') return speculative;
+	if (slug.toLowerCase() === 'beyond-humans') return beyondHumans;
+	if (slug.toLowerCase() === 'disreappearingwaters') return disreappearingWaters;
 };
+
 
 export default {
 	getIcon
